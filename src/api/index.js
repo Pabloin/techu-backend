b81ff28d82133f8422dc20b9213595ee085e9167
Pabloin <pablo.inchausti.bf@gmail.com>
2019-09-02 00:44:37 -0300

    ID #5 Swagger FIX Redirect

--- src/api/index.js
+++ src/api/index.js
@@ -22,9 +22,10 @@ app.use(cookieParser());
  */
 app.use('/swagger.yaml', express.static(__dirname + '/../../swagger.yaml'));
 app.use('/swagger-editor', express.static(__dirname + '/../../swagger-editor'));
-app.use('/', (req, res, next) => {
-  res.redirect('/swagger-editor?url=/swagger.yaml');
-});
+app.use('/', express.static(__dirname + '/../../swagger-editor'));
+// app.use('/', (req, res, next) => {
+//   res.redirect('/swagger-editor?url=/swagger.yaml');
+// });
 
 /**
  * Setup
