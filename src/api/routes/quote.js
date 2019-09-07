9bfa647c141d39ed51b2ce5e899445f4b93cbf94
Pabloin <pablo.inchausti.bf@gmail.com>
2019-09-07 15:09:23 -0300

    ID-#14 Fix de la transferencia entre cuentas

--- src/api/routes/quote.js
+++ src/api/routes/quote.js
@@ -38,6 +38,11 @@ router.get('/api-bcra/usd_uf/:select?', async (req, res, next) => {
   try {
 
     var tokenBCRA = Common.getTokenApiBCRA_Config();
+    var mockBCRA = process.env.Techu_TOKEN_BCRA_API_MOCK
+    if (mockBCRA) {
+      throw Error('Mock BCRA Enabled')
+    }
+
     var reqOptions = {
       url: 'https://api.estadisticasbcra.com/usd_of',
       method: 'GET',
@@ -58,20 +63,9 @@ router.get('/api-bcra/usd_uf/:select?', async (req, res, next) => {
          res.status(response.statusCode).send(`Code 403: Error Límite de 100 invocaciones x día del BCRA`);
          return;
        }
-
-       arrRta = JSON.parse(body)
-
-       console.log(`API BCRA OK ${arrRta.length}`)
-
-       let dolarData = (options.select === 'last') ? arrRta[arrRta.length - 1] : arrRta
-
-       console.log(`/api-bcra/usd_uf (${JSON.stringify(options)}) dolarData=${arrRta[arrRta.length - 1]}`)
      
-       let rta =  {
-         status: response.statusCode,
-         data: dolarData
-       };
-     
+       let rta = getRtaBCRA(options, arrRta);
+
        res.status(response.statusCode).send(dolarData);
      
        console.log(`/api-bcra/usd_uf (${JSON.stringify(options)})`)
@@ -79,38 +73,47 @@ router.get('/api-bcra/usd_uf/:select?', async (req, res, next) => {
     });
 
   } catch (e) {
-
-    // Por el caso el limite de consumo de 100 request x dia por Tocken
-    let arrRta_Mock = [
-      { "d": "2019-08-26", "v": 55.34 },
-      { "d": "2019-08-27", "v": 55.71 },
-      { "d": "2019-08-28", "v": 58.08 },
-      { "d": "2019-08-29", "v": 58.2 },
-      { "d": "2019-08-30", "v": 59.08 },
-      { "d": "2019-09-02", "v": 57.09 },
-      { "d": "2019-09-03", "v": 55.78 }
-    ];
-
-    arrRta = arrRta_Mock;
-
     console.log(`API BCRA ERROR: Uso Mock ${arrRta.length} -> error: ${e}`)
 
-    let dolarData = (options.select === 'last') ? arrRta[arrRta.length - 1] : arrRta
-
-    console.log(`/api-bcra/usd_uf (${JSON.stringify(options)}) dolarData=${dolarData}`)
-  
-    let rta =  {
-      status: response.statusCode,
-      data: dolarData
-    };
+    let rta = getRtaBCRA_Mock(options);
   
     res.status(response.statusCode).send(dolarData);
   
     console.log(`/api-bcra/usd_uf (${JSON.stringify(options)})`)
-
   }
 
 });
 
+getRtaBCRA_Mock = (options) => {
+  console.log(`getRtaBCRA_Mock(${JSON.stringify(options)})`)
+  // Por el caso el limite de consumo de 100 request x dia por Tocken
+  let arrRta_Mock = [
+    { "d": "2019-08-26", "v": 55.34 },
+    { "d": "2019-08-27", "v": 55.71 },
+    { "d": "2019-08-28", "v": 58.08 },
+    { "d": "2019-08-29", "v": 58.2 },
+    { "d": "2019-08-30", "v": 59.08 },
+    { "d": "2019-09-02", "v": 57.09 },
+    { "d": "2019-09-03", "v": 55.78 }
+  ];
+
+  return getRtaBCRA(options, arrRta_Mock)
+}
+
+getRtaBCRA = (options, arrRta) => {
+  console.log(`getRtaBCRA(${JSON.stringify(options)}, ${arrRta.length})`)
+  
+  let dolarData = (options.select === 'last') ? arrRta[arrRta.length - 1] : arrRta
+
+  console.log(`/api-bcra/usd_uf (${JSON.stringify(options)}) dolarData=${dolarData}`)
+
+  let rta =  {
+    status: 200,
+    data: dolarData
+  };
+
+  return rta
+
+}
 
 module.exports = router;
